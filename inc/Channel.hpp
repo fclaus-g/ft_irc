fce17a2b1421bbb80e8360c7fc2ce75190dffc29
Fernando Claus Gamez <fclaus-g@student.42malaga.com>
2024-10-11 13:23:09 +0200

    feat: channel: add attributes and method on initial step

--- inc/Channel.hpp
+++ inc/Channel.hpp
@@ -2,26 +2,67 @@
 #define CHANNEL_HPP
 
 #include "Server.hpp"
-
+/** + Debe poder autenticarse, establecer un apodo, un nombre de usuario, unirse a un canal, enviar y recibir mensajes privados utilizando el cliente de referencia.
+	+ Todos los mensajes enviados desde un cliente a un canal deben reenviarse a todos los demás clientes que se hayan unido al canal.
+	+ Debe tener operadores y usuarios regulares.
+	+ Luego, debe implementar los comandos que son específicos para los operadores de canal:
+		* KICK: expulsa a un cliente del canal
+		* INVITE: invita a un cliente a un canal
+		* TOPIC: cambia o visualiza el tema del canal
+		* MODE: cambia el modo del canal:
+			* i: establece o elimina el canal solo para invitados
+			* t: establece o elimina las restricciones del comando TOPIC para los operadores de canal
+			* k: establece o elimina la clave del canal (contraseña)
+			* o: otorga o quita privilegios de operador de canal
+			* l: Establecer/eliminar el número de users para el canal.*/
 class Channel
 {
-    private:
-        std::string _name;
-        std::string _topic;
-        std::vector<int> _users;
-    public:
-        Channel();
-        Channel(std::string name);
-        ~Channel();
-        
-        const std::string& getName() const;
-        const std::string& getTopic() const;
-        const std::vector<int>& getUsers() const;
+	private:
+		bool        _isPrivate;
+		size_t      _maxUsers;
+		std::string _channelName;
+		std::string _channelPass;
+		std::string _topic;
+		std::map<User&, bool> _users;
+		std::vector<User> _channelUsers;
+		std::vector<User> _channelOp;
+	public:
+		Channel();
+		Channel(std::string name);
+		Channel(const Channel& rhs);
+		Channel& operator=(const Channel& rhs);
+		~Channel();
+		
+		const bool& getIsPrivate() const;
+		const size_t& getMaxUsers() const;
+		const std::string& getName() const;
+		const std::string& getPass() const;
+		const std::string& getTopic() const;
+		const std::vector<User>& getUsers() const;
+		const std::vector<User>& getOp() const;
 
-        void setName(const std::string& name);
-        void setTopic(const std::string& topic);
-        void addUser(int userFd);
-        void removeUser(int userFd);
+		void setIsPrivate(bool isPrivate);
+		void setMaxUsers(size_t maxUsers);
+		void setName(const std::string& name);
+		void setPass(const std::string& pass);
+		void setTopic(const std::string& topic);
+		
+		
+		void addUserChannel(int userFd);
+		void removeUserChannel(int userFd);
+		void addOpChannel(int userFd);
+		void removeOpChannel(int userFd);
+		void kickUserChannel(int userFd);
+		void inviteUserChannel(int userFd);
+		void changeTopicChannel(const std::string& topic);
+		void changeModeChannel(const std::string& mode);
+		void changePassChannel(const std::string& pass);
+		void changeMaxUsersChannel(size_t maxUsers);
+		void changeNameChannel(const std::string& name);
+		void printUsersChannel();
+		void printOpsChannel();
 };
+
+std::ostream& operator<<(std::ostream& out, const Channel& channel);
 
 #endif
--- src/Channel.cpp
+++ src/Channel.cpp
@@ -2,77 +2,123 @@
 
 Channel::Channel()
 {
-    std::cout << "Channel object created" << std::endl;
+	this->_channelName = "default";
+	this->_topic = "";
+	this->_isPrivate = false;
+	this->_maxUsers = 0;
+	this->_channelPass = "";	
+	std::cout << "Channel object created" << std::endl;
 }
 
 Channel::Channel(std::string name)
 {
-    std::cout << "Channel object created" << std::endl;
-    this->_name = name;
+	std::cout << "Channel object created" << std::endl;
+	this->_channelName = name;
+	this->_topic = "";
+	this->_isPrivate = false;
+	this->_maxUsers = 0;
+	this->_channelPass = "";
 }
 
 Channel::~Channel()
 {
-    std::cout << "Channel object destroyed" << std::endl;
+	std::cout << "Channel object destroyed" << std::endl;
+}
+
+Channel::Channel(const Channel &rhs)
+{
+	std::cout << "Channel copy constructor" << std::endl;
+	*this = rhs;
+}
+
+Channel& Channel::operator=(const Channel &rhs)
+{
+	std::cout << "Channel assignment operator" << std::endl;
+	if (this != &rhs)
+	{
+		this->_channelName = rhs._channelName;
+		this->_topic = rhs._topic;
+		this->_isPrivate = rhs._isPrivate;
+		this->_maxUsers = rhs._maxUsers;
+		this->_channelPass = rhs._channelPass;
+		this->_channelUsers = rhs._channelUsers;
+		this->_channelOp = rhs._channelOp;
+	}
+	return *this;
+}
+
+
+
+
+/*-----------------------[GETTER]------------------------*/
+
+const bool& Channel::getIsPrivate() const
+{
+	return this->_isPrivate;
+}
+const size_t& Channel::getMaxUsers() const
+{
+	return this->_maxUsers;
+}
+const std::string& Channel::getName() const
+{
+	return this->_channelName;
+}
+const std::string& Channel::getPass() const
+{
+	return this->_channelPass;
+}
+const std::string& Channel::getTopic() const
+{
+	return this->_topic;
+}
+const std::vector<User>& Channel::getUsers() const
+{
+	return this->_channelUsers;
+}
+const std::vector<User>& Channel::getOp() const
+{
+	return this->_channelOp;
 }
 
 /*-----------------------[SETTER]------------------------*/
 
+void Channel::setIsPrivate(bool isPrivate)
+{
+	this->_isPrivate = isPrivate;
+}
+
+void Channel::setMaxUsers(size_t maxUsers)
+{
+	this->_maxUsers = maxUsers;
+}
 void Channel::setName(const std::string& name)
 {
-    this->_name = name;
+	this->_channelName = name;
 }
-
+void Channel::setPass(const std::string& pass)
+{
+	this->_channelPass = pass;
+}
 void Channel::setTopic(const std::string& topic)
 {
-    this->_topic = topic;
-}
-
-/*-----------------------[GETTER]------------------------*/
-
-const std::string& Channel::getName() const
-{
-    return this->_name;
-}
-
-const std::string& Channel::getTopic() const
-{
-    return this->_topic;
-}
-
-const std::vector<int>& Channel::getUsers() const
-{
-    return this->_users;
+	this->_topic = topic;
 }
 
 /*-----------------------[METHODS]------------------------*/
 
-void Channel::addUser(int userFd)
-{
-    this->_users.push_back(userFd);
-}
 
-void Channel::removeUser(int userFd)
-{
-    for (size_t i = 0; i < this->_users.size(); i++)
-    {
-        if (this->_users[i] == userFd)
-        {
-            this->_users.erase(this->_users.begin() + i);
-            return ;
-        }
-    }
-}
+
 
 std::ostream& operator<<(std::ostream& os, const Channel& channel)
 {
-    os << "Channel name: " << channel.getName() << std::endl;
-    os << "Channel topic: " << channel.getTopic() << std::endl;
-    os << "Channel users: ";
-    for (size_t i = 0; i < channel.getUsers().size(); i++)
-    {
-        os << channel.getUsers()[i] << " ";
-    }
-    os << std::endl;
-    return os;
+	os << "Channel name: " << channel.getName() << std::endl;
+	os << "Channel topic: " << channel.getTopic() << std::endl;
+	os << "Channel users: ";
+	for (size_t i = 0; i < channel.getUsers().size(); i++)
+	{
+		os << channel.getUsers()[i] << " ";
+	}
+	os << std::endl;
+	return os;
 }
