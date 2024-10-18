6e87977127f49e0f08c63e1287eb5e655d1d1b99
fclaus-g <fclaus-g@student.42malaga.com>
2024-10-18 17:38:08 +0200

    refactor: server.hpp

--- inc/Server.hpp
+++ inc/Server.hpp
@@ -24,14 +24,14 @@ class User;
 class Server
 {
 	private:
-		int						_port;
-		std::string				_password;
-		std::string				_name;
-		bool					_isRunning;
-		int						_serverFd;
-		std::string				_message;
-		std::vector<Channel>	_channels;//vector of channels
-		std::map<int, User *>	_users;//map of clients file descriptors and their objects
+		int							_port;
+		std::string					_password;
+		std::string					_name;
+		bool						_isRunning;
+		int							_serverFd;
+		std::string					_message;
+		std::vector<Channel>		_channels;//vector of channels
+		std::map<int, User*>		_users;//map of clients file descriptors and their objects
 		//std::map<int, std::string> clients;//map of clients file descriptors and their names
 		std::vector<struct pollfd> 	_fds;//pollfd used for monitoring file descriptors
 		std::string					_tempNick;
@@ -42,15 +42,15 @@ class Server
 		Server();
 		Server(int port, std::string password);
 		~Server();
-		
-		int getPort() const;
-		std::string getName() const;
-		bool getIsRunning() const;
-		void addChannel(std::string &name);
-		void start();
-		void run();
-		void stop();
-		void prepareSocket();
+		//Getters and setters
+		int			getPort() const;
+		std::string	getName() const;
+		bool		getIsRunning() const;
+		//Server methods
+		void		start();
+		void		run();
+		void		stop();
+		void		prepareSocket();
 		//User methods
 		void 		newConnection();
 		void		deleteUser(int socketFd);
@@ -63,27 +63,28 @@ class Server
 		void		parseMsg(int userFd, std::string msg);
 		bool		checkCmd(int userFd, std::string msg);
 		void		runCmd(int userFd, int key, std::string cmd);
-		void printMap(const std::map<int, User>& map);
-		void printVector(const std::vector<Channel>& vector);
 		//Channel methods
-		void createChannel(const std::string& name);
-		void addUserToChannel(const std::string& channelName, User& user);
-		void removeChannel(const std::string& name);
+		void 		createChannel(const std::string& name);
+		void 		addUserToChannel(const std::string& channelName, User& user);
+		void 		removeChannel(const std::string& name);
+		//void addChannel(std::string &name);
 
-		
-		void checkCommand(User user);
-		void commandUser(User user);
-		void commandNick(User user);
-		void commandJoin(User user);
-		void commandQuit(User user);
-		void commandPrivmsg(User user);
-		void commandKick(User user);
-		void commandInvite(User user);
-		void commandTopic(User user);
-		void commandMode(User user);
+		//Commands
+		void 		checkCommand(User user);
+		void 		commandUser(User user);
+		void 		commandNick(User user);
+		void 		commandJoin(User user);
+		void 		commandQuit(User user);
+		void 		commandPrivmsg(User user);
+		void 		commandKick(User user);
+		void 		commandInvite(User user);
+		void 		commandTopic(User user);
+		void 		commandMode(User user);
 
 		static void signalHandler(int signal);
-
+		//methods for debugging
+		void		printMap(const std::map<int, User>& map);
+		void		printVector(const std::vector<Channel>& vector);
 };
 
 std::ostream& operator<<(std::ostream& out, const Server& server);
