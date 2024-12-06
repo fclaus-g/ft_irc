dd7b9d61bb22100ccdca40d4920e7605af38985e
fclaus-g <fclaus-g@student.42malaga.com>
2024-12-06 21:00:09 +0100

    feat:command  JOIN works with /join #c1,#c2

--- src/cmdJoin.cpp
+++ src/cmdJoin.cpp
@@ -1,5 +1,12 @@
 
 #include "ft_irc.hpp"
+/*
+IRC usa una serie de códigos numéricos para indicar el estado de los comandos. Para JOIN, estos son algunos de los mensajes que deberás enviar desde el servidor al cliente:
+
+001 (RPL_WELCOME): Al usuario al conectarse al servidor.
+332 (RPL_TOPIC): Devuelve el tema del canal si tiene uno.
+353 (RPL_NAMREPLY): Muestra la lista de usuarios en el canal.
+366 (RPL_ENDOFNAMES): Indica el fin de la lista de usuarios.*/
 /*  JOIN #foobar                    ; join channel #foobar.
 
   JOIN &foo fubar                 ; join channel &foo using key "fubar".
@@ -39,23 +46,17 @@ void printVector(std::vector<std::string> args)
 	}
 }
 /**
- * @brief Command to join a channel, modification of channel_fclaus-g branch Server method
+ * @brief Command to join a channel
+ * 	- if the user is not authenticated, kick the user
+ * 	- split the message into the channel name and key
+ * 	- if the channel name is empty or does not start with #, send an error message
+ * 	- check if the channel exists
  *	- if the channel does not exist, create it and add the user as operator
  *	- if the channel exists, add the user to the channel
- * (!) Diff from original: Command._msg is the message received to parse
- * (!) Diff from original: Added first check if user is authenticated
- * (!) Diff from original: Addeded getter methods for channels vector and map
- *
- * 
  * TODO: Channel related methods have been left in Server as original; move them to Command?
+ * TODO: VERIFICAR SI EL CANAL ESTA EN MODO PRIVADO - mirar como esta MODE y como se gestiona
+ * TODO: VERIFICAR SI EL USUARIO HA SIDO INVITADO - mirar como esta INVITE y como se gestiona
  */
-/*
-IRC usa una serie de códigos numéricos para indicar el estado de los comandos. Para JOIN, estos son algunos de los mensajes que deberás enviar desde el servidor al cliente:
-
-001 (RPL_WELCOME): Al usuario al conectarse al servidor.
-332 (RPL_TOPIC): Devuelve el tema del canal si tiene uno.
-353 (RPL_NAMREPLY): Muestra la lista de usuarios en el canal.
-366 (RPL_ENDOFNAMES): Indica el fin de la lista de usuarios.*/
 void Command::cmdJoin()
 {
 	if (this->_msg.find("JOIN") == std::string::npos)
@@ -74,36 +75,37 @@ void Command::cmdJoin()
 	std::vector<std::string> keys = (args.size() > 2) ? splitMessage(args[2], ',') : std::vector<std::string>();
 	printVector(keys);
 
-
-	std::string channelName = this->_msg.substr(this->_msg.find("JOIN") + 5);
-	channelName.erase(channelName.find_last_not_of(" \n\r\t") + 1);
-
-	if (channelName.empty() || channelName[0] != '#'){
-		return (this->_server.sendWarning(this->_user.getFd(),
-				"JOIN: Error: No such nick/channel\n"));}
-	Channel *channel = this->_server.getChannelByName(channelName);
-	if (!channel)
+	for (size_t i = 0; i < channels.size(); i++)
 	{
-		std::cout << "Channel does not exist, creating new channel" << std::endl;
-		this->_server.createChannel(channelName);
-		channel = this->_server.getChannelByName(channelName);
+		std::string channelName = channels[i];
+		channelName.erase(channelName.find_last_not_of(" \n\r\t") + 1);
+
+		if (channelName.empty() || channelName[0] != '#'){
+			return (this->_server.sendWarning(this->_user.getFd(),
+					"JOIN: Error: No such nick/channel\n"));}
+		Channel *channel = this->_server.getChannelByName(channelName);
 		if (!channel)
 		{
-			std::cerr << "Failed to create or retrieve channel" << std::endl;
-			return;
+			std::cout << "Channel does not exist, creating new channel" << std::endl;
+			this->_server.createChannel(channelName);
+			channel = this->_server.getChannelByName(channelName);
+			if (!channel)
+			{
+				std::cerr << "Failed to create or retrieve channel" << std::endl;
+				return;
+			}
+			channel->addUserChannel(this->_user);
+			channel->addOpChannel(this->_user);
 		}
-		//this->_server.addUserToChannel(channelName, this->_user);
-		channel->addUserChannel(this->_user);
-		channel->addOpChannel(this->_user);
+		else
+		{
+			std::cout << "Channel exists, adding user to channel" << std::endl;
+			if (channel->isUserInChannel(this->_user))
+				return (this->_server.sendWarning(this->_user.getFd(),
+					"JOIN: Error: You are already in that channel\n"));
+			channel->addUserChannel(this->_user);
+			std::string msg = "JOIN " + channelName + "\n";
+			channel->broadcastMessage(msg, this->_user);
+		}
 	}
-	else
-	{
-		std::cout << "Channel exists, adding user to channel" << std::endl;
-		if (channel->isUserInChannel(this->_user))
-			return (this->_server.sendWarning(this->_user.getFd(),
-				"JOIN: Error: You are already in that channel\n"));
-		channel->addUserChannel(this->_user);
-		std::string msg = "JOIN " + channelName + "\n";
-		channel->broadcastMessage(msg, this->_user);
-	}
-}
+}	
